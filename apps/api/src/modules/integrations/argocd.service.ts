449865c57e13c69a1c4e33a6e5b48a6b93f62514
Weyder <weyderfs@gmail.com>
2026-05-11 14:10:44 -0300

    feat(deploy): Create the Helm Chart v0

--- apps/api/src/modules/integrations/argocd.service.ts
+++ apps/api/src/modules/integrations/argocd.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-} from "@nestjs/common";
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { firstValueFrom } from "rxjs";
 import { IntegrationCredentialService } from "./integration-credential.service";
