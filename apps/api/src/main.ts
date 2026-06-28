599bc30a98b71352e517836d189ba419b8388d49
Weyder <weyderfs@gmail.com>
2026-06-28 00:00:20 -0300

    refactor(api): remove enableImplicitConversion from global ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -102,9 +102,6 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
     }),
   );
 
